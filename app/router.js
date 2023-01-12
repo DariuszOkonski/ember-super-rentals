dc6a9271d19c5f31df007b1dd4b4d90496b22396
Dariusz Okoński <darek200180@gmail.com>
2023-01-12 19:20:47 +0100

    add share-button

--- app/router.js
+++ app/router.js
@@ -12,4 +12,4 @@ Router.map(function () {
   this.route('rental', { path: '/rentals/:rental_id' });
 });
 
-// tutorial->part 2-> servide injection ->
+// tutorial->part 2-> servide injection -> ember services vs. global variables
